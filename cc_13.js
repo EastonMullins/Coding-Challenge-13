45d2ea6d62bf0fee476e32fa0a19dd929a795744
Easton <eastonmullins@usf.edu>
2025-03-09 17:07:28 -0400

    Task 2 - Adding Employee Cards Dynamically

---
+++ cc_13.js
@@ -0,0 +1,40 @@
+// Task 1 - Creating Base Setup
+
+// On HTML file
+
+// Task 2 - Adding Employee Cards Dyanimcally
+
+function addEmployeeCard(name, position) {
+    const employeeContainer = document.getElementById("employeeContainer");
+
+    const employeeCard = document.createElement("div");
+    employeeCard.setAttribute("class","employee-card");
+
+    const nameElement = document.createElement("h2");
+    nameElement.textContent = name;
+
+    const positionElement = document.createElement("p");
+    positionElement.textContent = position;
+
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.classList.add("remove-button");
+
+    removeButton.addEventListener("click", function () {
+        employeeContainer.removeChild(employeeCard);
+    });
+
+    // Adding Elements to Employee Card
+
+    employeeCard.appendChild(nameElement);
+    employeeCard.appendChild(positionElement);
+    employeeCard.appendChild(removeButton);
+
+    //Add Card to Container
+    
+    employeeContainer.appendChild(employeeCard);
+
+}
+
+addEmployeeCard("Colby Mullins", "Awesome Camper");
+addEmployeeCard("Alice Clark", "Cat Farmer");
